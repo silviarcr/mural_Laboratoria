0467b900f96b109b1d2524c2135ea273b3f33c13
silvia.rcr <silvia.rcr@gmail.com>
2018-04-06 03:25:47 -0300

    validacao e autenticacao de login firebase

--- js/login.js
+++ js/login.js
@@ -1,27 +1,60 @@
 
+var emailIsValid = false;
+var passwordIsValid = false;
 
 function validate() {
+
     var email = document.getElementById("login-name").value;
     var password = document.getElementById("login-pass").value;
-    if (isEmpty(password))
+    if (isEmpty(password)) {
         displaySpan('isPassword', 'Favor digitar senha!');
-
+        passwordIsValid = false;
+    }
+    else {
+        passwordIsValid = true;
+    }
     checkEmail(email);
 
-    if (email == "formget@formget.com" && password == "formget#123") {
-        alert("Login successfully");
-        window.location = "mural.html"; // Redirecting to other page.
+
+    if (!passwordIsValid || !emailIsValid) {
         return false;
+    }
+    else {
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(email, password);
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === 'auth/wrong-password') {
+                displaySpan('isPassword', 'Senha incorreta!');
+                return false;
+            }
+            if (errorCode === 'auth/user-not-found') {
+                displaySpan('isEmail', 'Usuário inválido');
+                return false;
+            }
+            if (errorCode !== 'auth/user-not-found' && errorCode !== 'auth/wrong-password') {
+                window.location = "mural.html"; // Redirecting to other page.
+            }
+        });
     }
 }
 
 function checkEmail(mail) {
-    if (isEmpty(mail))
+    if (isEmpty(mail)) {
         displaySpan('isEmail', 'Favor digitar email!');
+        emailIsValid = false;
+    }
     else {
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-        if (!reg.test(mail))
+        if (reg.test(mail)) {
+            emailIsValid = true;
+        }
+        else {
             displaySpan('isEmail', 'Email inválido!');
+            emailIsValid = false;
+        }
     }
 }
 
